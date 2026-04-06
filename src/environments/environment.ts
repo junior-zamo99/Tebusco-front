1752f5e0318a02df25d93e4e28d75cd7f0383e92
JuniorZamoAdunay <86460391+junior-zamo99@users.noreply.github.com>
2026-04-06 13:18:59 -0400

    ssl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://66.94.104.52:3000/api',
+  //apiUrl: 'http://66.94.104.52:3000/api',
   //apiUrl: 'http://192.168.88.111:3000/api',
-  backendUrl: 'http://66.94.104.52:3000'
+  //backendUrl: 'http://66.94.104.52:3000'
   //backendUrl: 'http://192.168.88.73:3000'
-  //apiUrl: 'http://localhost:3000/api',
-  //backendUrl: 'http://localhost:3000'
+  apiUrl: 'https://api.tebusco.net/api',
+  backendUrl: 'https://api.tebusco.net:3000'
 };
